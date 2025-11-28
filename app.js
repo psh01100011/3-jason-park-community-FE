ee6da569e17ecc4f0b560dc98f8dbe72b03c6656
jason <eofpsh@gmail.com>
2025-11-28 13:22:45 +0900

    tests 추가 및 패키지 추가

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/assets')));
 
+app.get('/cicdtest', (req, res) => {
+  res.send('Hello, World!');
+});
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/pages/index/index.html');
 });
